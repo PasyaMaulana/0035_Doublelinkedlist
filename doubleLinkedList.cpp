2061172721f69dd27214c19dc3d3641cda7232e6
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-15 10:39:24 +0700

    step 1 method addNode dengan mengalokasikan memori untuk node baru

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -27,5 +27,8 @@ public:
         string nm;
         cout << "\nEnter the roll number of the student";
         cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
     }
 };
